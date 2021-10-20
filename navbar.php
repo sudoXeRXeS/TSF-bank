39a5eb57ef1f2f0f70065f06031094dac4583ac7
Naushad Alam <72906944+sudoXeRXeS@users.noreply.github.com>
2021-10-20 14:01:16 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,20 @@
+<!-- navbar --> 
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+      <h3> <a class="navbar-brand" href="index.php" style="color : #DA0037;"><b>TSF BANK</b></a> </h3>
+      <img src="images/bank.jpg" width="50" height="70" class="img-fluid pt-2">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+               <h3> <a class="nav-link" href="index.php" style="color : #DA0037;"><b>Home</b></a></h3>
+              </li>
+              <li class="nav-item">
+              <h3> <a class="nav-link" href="transfermoney.php" style="color : #DA0037;"><b>View Customers</b></a></h3>
+              </li>
+              <li class="nav-item">
+              <h3> <a class="nav-link" href="transactionhistory.php" style="color : #DA0037;"><b>View Transaction</b></a></h3>
+              </li>
+          </div>
+       </nav>
